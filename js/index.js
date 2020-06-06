4b62c2ad1479cda8f7e05ae983f8e194bf432194
Amandeep-Bhullar <er.amandeepbhullar@gmail.com>
2020-06-05 23:15:48 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const anArray=[];
+anArray.push('Hello');
+console.log(`First manual value is: `+anArray);
+anArray[3]='World';
+console.log(`On the third index: `+anArray);
+
+console.log(`This Array's length is: `+anArray.length);
+
+console.log(`This Array is: `+anArray[1]);
